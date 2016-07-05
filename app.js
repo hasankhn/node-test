fddb08aae03a4946586344ee5a1b08cceff784ec
Hassan Karim Khan <hasankhn06@gmail.com>
2016-07-05 16:25:12 +0500

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express'),
-    app = express(),
+    promise = require('bluebird'),
+    request = require('request'),
+    syncRequest = require('sync-request'),
     _ = require('underscore'),
+    fs = require('fs'),
+    extractor = require('unfluff'),
+    app = express(),
     port = 3000 || process.env.PORT;
 
 app.use('/', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-require('./routes/routes.js')(app, express, _);
+require('./routes/routes.js')(app, express, _, promise,request,extractor,fs,syncRequest);
 
 app.listen(port, function() {
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,46 @@
-module.exports = function(app, express, _) {
+module.exports = function(app, express, _, promise, request, extractor, fs, sync) {
+
     router = express.Router();
+    var template = [];
+
+    function getUri(query, callback) {
+        var i = 0;
+        var body = '';
+        //iterating through query paramater
+        while (i < query.length) {
+            body = (sync('GET', "http://" + query[i]));
+            template.push(body.getBody().toString());
+            i++;
+        }
+        //passing data to callback 
+        callback(template);
+        template = [];
+    }
     router.get('/I/want/title', function(req, res) {
-        console.log(req.query);
         if (_.isEmpty(req.query)) {
             res.status(404).send('Please specify your query');
         }
         var query = req.query.address;
-        if (typeof query == 'string') {
+        if (typeof query === 'string') {
             query = [query]
         }
-        console.log(query);
-        res.render('index', { name: 'CareMerge Test', query: query });
+        //passing query to a callback function
+        getUri(query, function(data) {
+            var title = [];
+            var myData = '';
+            // iterating through data coming from callback
+            data.forEach(function(item) {
+            //extracting title using unfluff library
+                myData = extractor(item);
+                //pushing title into array
+                title.push(myData.title);
+            });
+            //rendering it on html 
+            res.render('index', { name: 'CareMerge Test', query: query, data: title });
+            //emptying title array for next call
+            title = [];
+        });
+
     });
     app.use('/', router);
 }
